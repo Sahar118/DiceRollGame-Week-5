c26658cf024cf882ff48caeebc8710937eeda3bd
sahar118 <sahar.t118@gmail.com>
2023-01-29 03:12:31 +0200

    add files

--- Code Files/script.js
+++ Code Files/script.js
@@ -16,9 +16,11 @@ const winner1=  document.querySelector('#winCount-1');
 const modal = document.getElementById("myModal");
 const newGameBtn1 = document.getElementById("new-game-btn");
 const startGameBtn = document.getElementById("letStart");
-const turnOffSound = document.querySelector('.turn-off-sound')
-const textPlayerWon0= document.querySelector('.player-0-won-text')
-const textPlayerWon1= document.querySelector('.player-1-won-text')
+const turnOffSound = document.querySelector('.turn-off-sound');
+const textPlayerWon0= document.querySelector('.player-0-won-text');
+const textPlayerWon1= document.querySelector('.player-1-won-text');
+const spanDouble6 = document.querySelector('.double-6');
+const spanDouble3 = document.querySelector('.double-3');
 
 
 
@@ -28,22 +30,25 @@ let cong_Sound = new Audio('/audio/TCD25PS-game-success (1).mp3');
 cong_Sound.volume = 0.1;
 
 
-
 // 
 scorePlayer0.textContent=0;
 scorePlayer1.textContent=0;
 scoreCurrent0.textContent=0;
 scoreCurrent1.textContent=0;
 
+
+// Functions
 startGameBtn.onclick = function(){
     modal.style.display='none';
 };
 
-startGameBtn.addEventListener('click', function(e){
-    targetScore = document.getElementById('targetScore').valueAsNumber;
+startGameBtn.addEventListener("click", function (e) {
+    targetScore = document.getElementById("targetScore").valueAsNumber;
 });
-let score, playerNum, currentScore1, playingStatus;
-// 
+
+newGameBtn.addEventListener('click', startNewGame);
+
+let score, playerNum, currentScore1, playingStatus, targetScore;
 const startNewGame = function (){
     score =[0,0];
     playerNum = 0;
@@ -53,7 +58,6 @@ const startNewGame = function (){
     scorePlayer1.textContent=0;
     scoreCurrent0.textContent=0;
     scoreCurrent1.textContent=0;
-    
     diceTop.classList.add('hidden');
     diceBottom.classList.add('hidden');
     player0.classList.add('player-active');
@@ -61,12 +65,13 @@ const startNewGame = function (){
     player0.classList.remove('winner');
     player1.classList.remove('winner');
     POP_SOUND.play();
-
-}
-startNewGame()
-const switchPlayer = function(){
     textPlayerWon1.style.display = 'none';
     textPlayerWon0.style.overFlow = 'none';
+}
+startNewGame();
+
+
+const switchPlayer = function(){
     document.getElementById(`score-current-player-${playerNum}`).textContent = 0;
     currentScore1=0;
     playerNum = playerNum === 0 ? 1 : 0; 
@@ -77,46 +82,43 @@ const switchPlayer = function(){
     
 }
 
-
 const gameFun = rollDiceBtn.addEventListener('click', function(){
     if (playingStatus){
         const dice1 =Math.trunc(Math.random()*6) +1;
         const dice2 =Math.trunc(Math.random()*6) +1;
-        
         diceTop.classList.remove('hidden');
         diceTop.src =`/img/dice/dice-${dice1}.png`;
         diceBottom.classList.remove('hidden');
         diceBottom.src = `/img/dice/dice-${dice2}.png`;
-        cong_Sound.play();
         POP_SOUND.play();
-        
-        
-        
         if (dice1 === 3 && dice2 === 3){
             document.getElementById(`score-current-player-${playerNum}`).textContent = currentScore1;
             currentScore1=0;
-            cong_Sound.play();
+            spanDouble3.style.display= 'block';
             POP_SOUND.play();
-            
-            
+            setTimeout(function () {
+                spanDouble3.style.display = "none";
+            }, 1500);
         }else if(dice1 === 6 && dice2 === 6){
-            switchPlayer();
-            cong_Sound.play();
+            spanDouble6.style.display= 'block';
             POP_SOUND.play();
-            
-            
-
+            setTimeout(function () {
+                spanDouble6.style.display = "none";
+            }, 1500);
+            switchPlayer();
         }else{
             let sum = dice1 + dice2;
             currentScore1 += sum;
             document.getElementById(`score-current-player-${playerNum}`).textContent =currentScore1;
         }
-        cong_Sound.play();
         POP_SOUND.play();
-        
     }
 })
+
+
+
 let winNum = 0;
+
 const gameOve = holdBtn.addEventListener('click', function(){
     
     if(playingStatus){
@@ -125,17 +127,18 @@ const gameOve = holdBtn.addEventListener('click', function(){
         
         cong_Sound.play();
         
-        if (score[playerNum]<100){
+        if (score[playerNum]<targetScore){
             switchPlayer()
             cong_Sound.play();
             
         }
-        else if(score[playerNum]===100){
+        else if(score[playerNum] === targetScore){
             playingStatus=false;
             winNum += 1
             document.querySelector(`.player-${playerNum}-div`).classList.add('winner');
             document.querySelector(`.player-${playerNum}-div`).classList.remove('player-active');
             document.getElementById(`winCount-${playerNum}`).textContent = winNum;
+            `textPlayerWon${playerNum}`.style.display = 'block';
             cong_Sound.play();
             
         }
@@ -165,13 +168,6 @@ const gameOve = holdBtn.addEventListener('click', function(){
         }
         
     }})
-    newGameBtn.addEventListener('click', startNewGame);
-    
-    
 
 
-    
-    // newGameBtn1.onclick = function(){
-    //     window.location.reload();
-    // }
-    
+
